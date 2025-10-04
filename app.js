8d4e208ca9db57e346fc11dcade85e63e34b70cc
Mohammad Abdulhakim <duxwork0@gmail.com>
2025-10-04 23:38:44 +0300

    setup env

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { PORT } from "./config/env.js";
 
 const app = express();
 
@@ -6,8 +7,8 @@ app.get("/", (req,res)=>{
     res.send("Do not worry about your subscriptions end dates.")
 })
 
-app.listen(8080, ()=>{
-    console.log("Subscription Tracker API is running on http://localhost:8080")
+app.listen(PORT, ()=>{
+    console.log("Subscription Tracker API is running on http://localhost:"+PORT)
 })
 
 
