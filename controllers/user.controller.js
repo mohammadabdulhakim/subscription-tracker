986e1ece967d570b8760f5358c44cc7ccbbf6916
Mohammad Abdulhakim <duxwork0@gmail.com>
2025-10-06 15:19:25 +0300

    implement updateUser, deleteUser

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 
 export const getUsers = async (req,res, nxt) => {
@@ -27,17 +29,51 @@ export const getUserById = async (req,res, nxt) => {
 }
 
 
-export const updateUser = (req,res, nxt) => {
+export const updateUser = async (req,res, nxt) => {
     try{
+        const session = await mongoose.startSession();
+        session.startTransaction();
         
         const newUser = {};
+        
         if(req.body.name) newUser.name = req.body.name;
         if(req.body.email) newUser.email = req.body.email;
+
+        let updatedUser;
+        if(req.body.oldPassword && req.body.newPassword){
+            const user = await User.findById(req.userId);
+
+            const isMatch = await bcrypt.compare(req.body.oldPassword, user.password);
+
+            if(isMatch){
+                const salt = await bcrypt.genSalt(10);
+                const hashedPassword = await bcrypt.hash(req.body.newPassword, salt);
+                newUser.password = hashedPassword;
+            }else{
+                const error = new Error("Password is incorrect.");
+                error.statusCode = 401;
+                throw error;
+            }
+
+            await User.updateOne({_id: req.userId}, newUser, {session});
+            updatedUser = {...user._doc, ...newUser};
+        }else {
+            updatedUser = await User.findByIdAndUpdate(req.userId, newUser, {new: true, session});
+        }
         
-        const user = User.findByIdAndUpdate(req.params.id, newUser, {new: true}).select('-password');
+        await session.commitTransaction();
+        session.endSession();
 
+        res.status(200).json({success: true, data: updatedUser});
+    }catch(err){
+        nxt(err);
+    }
+}
 
-        res.status(200).json({success: true, data: user});
+export const deleteUser = async (req,res, nxt) => {
+    try{
+        await User.findByIdAndDelete(req.userId);
+        res.status(200).json({success: true, data: null});
     }catch(err){
         nxt(err);
     }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { getUserById, getUsers } from '../controllers/user.controller.js';
+import { deleteUser, getUserById, getUsers, updateUser } from '../controllers/user.controller.js';
 import authorize from '../middlewares/auth.middleware.js';
 
 const userRouter = Router();
@@ -7,15 +7,8 @@ const userRouter = Router();
 userRouter.get('/', getUsers);
 userRouter.get('/:id', authorize, getUserById);
 
-userRouter.post('/',(req,res)=>{
-    res.send({title: "Write new user"})
-});
-userRouter.put('/:id', authorize ,(req,res)=>{
-    res.send({title: "Update user"})
-});
-userRouter.delete('/:id',(req,res)=>{
-    res.send({title: "delete a user data"})
-});
+userRouter.put('/:id', authorize , updateUser);
+userRouter.delete('/:id', authorize, deleteUser);
 
 
 export default userRouter;
